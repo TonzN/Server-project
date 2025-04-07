3f72398d992d3fee570108a3d911b9fb4ca9cc67
TonzN <tonzgn@gmail.com>
2025-04-07 12:12:44 +0200

    d

--- server3/server_manager.py
+++ server3/server_manager.py
@@ -310,5 +310,6 @@ async def run_server():
     await server.wait_closed()  # Keeps the server running
 
 asyncio.run(server_pool.initialize())
-print(db_get_user_profile(get_connection(server_pool.get_pool("main_pool")), "0001"))
+pool = asyncio.run(server_pool.get_pool("main_pool"))
+print(db_get_user_profile(get_connection(pool), "0001"))
 asyncio.run(run_server())
