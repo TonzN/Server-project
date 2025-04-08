c176c225fd2f7896cea8f70332a108df423252af
TonzN <tonzgn@gmail.com>
2025-04-08 12:38:37 +0200

    d

--- server3/update_users.py
+++ server3/update_users.py
@@ -1,13 +1,14 @@
 import loads
 import database_manager
 import server_utils
+import asyncio
 
 loads.time.sleep(1)
 
 users = [database_manager.get_user_json_profile(user) for user in database_manager.get_all_users_json_profile()]
 ordered_users = server_utils.json_to_arr_ordered(users, ("username", "password", "id", "permission_level", "securitymode"))
 print(ordered_users)
-database_manager.db_add_multiple_user_profile(ordered_users)
+asyncio.run(database_manager.db_add_multiple_user_profile(ordered_users))
 
 
 update_json_data = False
